2446631cf779a6cc9c1bf20af24ef433145f6ef4
Gabriel E Penaranda G (GePTulkhas) <gabriel.e.p.gonzalez@gmail.com>
2018-11-19 23:05:23 -0400

    Agregado el sistema de authentication de Symfony mediante bcrypt

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+//use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        $data['last_username'] = $lastUsername;
+        $data['error'] = $error;
+        return $this->render('security/login.html.twig',$data);
+    }
+
+    /*
+    private function ejemploEncodePassword(UserPasswordEncoderInterface $encoder) {
+        
+        $em = $this->getDoctrine()->getManager();
+
+        $user = $em->getRepository(User::class)->find(1);
+
+        $plainPassword = '123456';
+    $encoded = $encoder->encodePassword($user, $plainPassword);
+        
+    
+    $user->setPassword($encoded);
+        $em->persist($user);
+        $em->flush();
+    }
+    */
+
+}
